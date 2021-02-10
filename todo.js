806bc49aac9a52ba450b6fe1cfb9cdb001acbd45
anna-ip <anna.almestal.com>
2021-02-10 15:33:20 +0100

    added a function to add todos and update/push to an array

--- todo.js
+++ todo.js
@@ -1,3 +1,22 @@
-// input text added to a list/array with the submit button 
-const todoList = []
+const userInput = document.getElementById("todo-input");
+const addBtn = document.getElementById("todo-btn");
+let list = document.getElementById("todo-list");
+
+// input text added to a list/array with the submit button
+let todoList = [];
+
+//get hold of the input value
+function outputValue() {
+  let newTodo = userInput.value;
+  list = document.getElementById("todo-list").innerHTML = newTodo;
+  todoList.push(list)
+  console.log('todoList',todoList)
+}
+
 // add eventlistner to the btn to update/push to the list/array
+addBtn.addEventListener("click", addTodo);
+
+function addTodo(list) {
+  //take the user input and add to the todoList
+  
+}
